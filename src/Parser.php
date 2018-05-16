e0b1457e8ef2041468eb20f462fdd29a37449af1
Dmitrii Antonov <d.antonovbox@gmail.com>
2018-05-17 01:16:16 +0300

    Init commit

---
+++ src/Parser.php
@@ -0,0 +1,36 @@
+<?php
+
+//require_once 'ParserInterface.php';
+
+namespace antonov\parser;  // немспейс обычно состоит из имени автора и названия, которое мы придумаем
+
+/**
+ * Description of Parser
+ *
+ * @author valerchik
+ */
+class Parser implements ParserInterface
+{
+	/**
+     * @param string $url
+     * @param string $tag
+     * @return array
+     */
+    public function process(string $url, string $tag): array 
+    {
+        $htmlPage = file_get_contents($url);
+		
+		if ($htmlPage === false) {
+			return ['Invalid URL'];
+		}
+		
+		preg_match_all('/<' . $tag . '.*?>(.*?)<\/' . $tag . '>/s', $htmlPage, $strings);
+		
+		if (empty($strings[1])) {
+			return ['There are no such tags on the page'];
+		}
+		
+		return $strings[1];
+    }
+
+}
---
+++ src/ParserInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace antonov\parser;  // немспейс обычно состоит из имени автора и названия, которое мы придумаем
+
+interface ParserInterface
+{
+    /**
+     * 
+     * @param string $url
+     * @param string $tag
+     * @return array
+     */
+    public function process(string $url, string $tag): array;
+}
